0ac763efab8277dcd04b005987f5870a16fd181f
samedan <same.dan@gmail.com>
2023-01-28 10:17:51 +0100

    POST store Customer

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Models\Customer;
 use Illuminate\Http\Request;
-use App\Filters\V1\CustomersFilter;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\StoreCustomerRequest;
 use App\Http\Resources\V1\CustomerResource;
-use App\Http\Requests\UpdateCustomerRequest;
 use App\Http\Resources\V1\CustomerCollection;
+use App\Filters\V1\CustomersFilter;
+use App\Http\Requests\V1\StoreCustomerRequest;
+
+// use App\Http\Requests\UpdateCustomerRequest;
+
+
 
 class CustomerController extends Controller
 {
@@ -57,7 +60,7 @@ class CustomerController extends Controller
      */
     public function store(StoreCustomerRequest $request)
     {
-        //
+        return new CustomerResource(Customer::create($request->all()));
     }
 
     /** // /api/customers/1234
