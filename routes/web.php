13766a30a6edb5cb2985f615cb3ff94f8ce58a15
Daniel Popescu <same.dan@gmail.com>
2023-02-02 14:06:04 +0100

    adminToken web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,28 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/setup', function() {
+    $credentials = [
+        'email' => 'admin@gmail.com',
+        'password' => 'password'
+    ];
+    if(!Auth::attempt($credentials)) {
+        $user = new User();
+
+        $user->name = 'Admin';
+        $user->email = $credentials['email'];
+        $user->password = Hash::make($credentials['Admin']);
+
+        $user ->save();
+
+        if(Auth::attempt($credentials)) {
+            $user = Auth::user();
+
+            $adminToken = $user->createToken('admin-token', ['create', 'update', 'delete']);
+            
+        }
+    }
 });
